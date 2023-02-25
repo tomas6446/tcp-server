bde7121f52b88b743d0435d3ae2ff0b26ae0b159
tomas6446 <tomas.kozakas@gmail.com>
2023-02-25 16:47:57 +0200

    Update client

--- client.c
+++ client.c
@@ -2,11 +2,73 @@
 // Created by tomas
 //
 
-int main() {
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define BUFFLEN 1024
+
+int main(int argc, char *argv[]) {
     char *ip = "127.0.0.1";
-    int port = 5566;
+    unsigned int port;
+
+    int sock = 0;
+    struct sockaddr_in addr;
+    socklen_t addr_size;
+    char buffer[BUFFLEN];
+
+    if (argc != 3) {
+        fprintf(stderr, "USAGE: %s <ip> <port>\n", argv[0]);
+        exit(1);
+    }
+    port = atoi(argv[2]);
+
+    if ((port < 1) || (port > 65535)) {
+        perror("[-] Invalid port specified.\n");
+        exit(1);
+    }
+
+#ifdef _WIN32
+    WSAStartup(MAKEWORD(2,2),&data);
+#endif
+    if (socket(AF_INET, SOCK_STREAM, 0) < 0) {
+        perror("[-] Socket error");
+        exit(1);
+    }
+    printf("[+] TCP server sock created.\n");
 
 
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
 
+#ifdef _WIN32
+    servaddr.sin_addr.s_addr = inet_addr(ip);
+#else
+    if (inet_aton(ip, &addr.sin_addr) <= 0) {
+        perror("[-] Invalid remote IP address.\n");
+        exit(1);
+    }
+#endif
+
+    if (connect(sock, (struct sockaddr *) &addr, sizeof addr) < 0) {
+        perror("[-] Error connecting to the server.\n");
+    }
+    printf("Connected to the server.\n");
+
+    bzero(buffer, 1024);
+    strcpy(buffer, "Hello, this is client.\n");
+    printf("Client: %s\n", buffer);
+    send(sock, buffer, strlen(buffer), 0);
+
+    bzero(buffer, 1024);
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    printf("Disconnected from the server.\n");
     return 0;
 }
--- server.c
+++ server.c
@@ -18,11 +18,12 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUFFLEN 1024
 
-int main() {
+int main(int argc, char *argv[]) {
 
     char *ip = "127.0.0.1";
-    int port = 5566;
+    int port;
 
     int server_socket;
     int client_socket;
@@ -30,36 +31,73 @@ int main() {
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t addr_size;
-    char buffer[1024];
+    char buffer[BUFFLEN];
     int n;
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
+    if (argc != 2) {
+        printf("USAGE: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    port = atoi(argv[1]);
+
+    if ((port < 1) || (port > 65535)) {
+        perror("[-] Invalid port specified.\n");
+        exit(1);
+    }
+
+#ifdef _WIN32
+    WSAStartup(MAKEWORD(2,2),&data);
+#endif
+    /*
+     * Creating socket
+     */
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("[-] Socket error");
         exit(1);
     }
     printf("[+] TCP server socket created.\n");
 
-
-    memset(&server_addr, 0, sizeof(server_addr));
+    /*
+     * Clean and fill up servers structure
+     */
+    memset(&server_addr, '\0', sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    n = bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if (n < 0) {
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+
+
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
         perror("[-] Bind error");
         exit(1);
     }
     printf("[+] Bind to the port number: %d\n", port);
 
-    listen(server_socket, 5);
-    printf("Listening...\n");
 
+    if(listen(server_socket, 5) < 0) {
+        perror("[-] Error listening.\n");
+        exit(1);
+    }
+
+    printf("Listening...\n");
     while (1) {
         addr_size = sizeof(client_addr);
         client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &addr_size);
         printf("[+] Client connected.\n");
+
+        bzero(buffer, 1024);
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        printf("Client: %s\n", buffer);
+
+        bzero(buffer, 1024);
+        strcpy(buffer, "Hi, this is server.\n");
+        printf("Server: %s\n", buffer);
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        close(client_socket);
+
+        printf("[+] Client disconnected.\n\n");
     }
 
     return 0;
